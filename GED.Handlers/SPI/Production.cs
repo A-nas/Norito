d9395da3da99c9a0aca107b2314a76dfc334242f
A-nas <anass.laghouaouta@hotmail.fr>
2017-10-31 18:32:54 +0100

    safe checking on updated  SF object added

--- GED.Handlers/SPI/Production.cs
+++ GED.Handlers/SPI/Production.cs
@@ -32,8 +32,6 @@ namespace GED.Handlers
             int nombreActes = actes.Count;
             Dictionary<string[], WsResponse> cresponses = new Dictionary<string[], WsResponse>();
             for (int i = 0; i < nombreActes; i++){
-            // if i pass TRANSTYPE TABLE here as method parameter, The context will depend on the company (unless TRANSTYPE table concerne all companies)
-                //Dynamic Dyspatching
                 IActe acteprod = new Spirica(actes[i]);
                 Dictionary<string[], WsResponse> currentResponse = new Dictionary<string[], WsResponse>();
                 currentResponse = await acteprod.sendProd(); // send one "Acte" *** (dic with one element)
@@ -45,7 +43,7 @@ namespace GED.Handlers
         }// must return boolean
 
         //update "acte" by "acte" this fucntion consume more time/space than updateSalesForceV1 but fix permission issue
-        // method must be splited for each attribute identifier
+        // method must be splited for each attribute identifier ♣ GERER L'exception de celui la
         public void updateSalesForce(Dictionary<string[],WsResponse> responses){
             // IDS
             string username = ConfigurationManager.AppSettings["loginSF"];
@@ -63,22 +61,25 @@ namespace GED.Handlers
                 //string soqlQuery = "SELECT Id, Commentaire_Interne__c, Statut_du_XML__c FROM Acte__c WHERE Name = '" + response.Key + "'"; OLD
                 string soqlQueryActe = "SELECT Id, Commentaire_XML__c, Statut_du_XML__c FROM Acte__c WHERE Name = '" + response.Key[0] + "'";
                 QueryResult result = SfService.query(soqlQueryActe);
-                if(result.size != 0) // must return one or zero
+                if (result.size != 0) {
                     SfActe = (Acte__c)result.records[0]; // take the only item selected
-                // update data
-                SfActe.Commentaire_XML__c = string.Join(" ", responses[response.Key].message);
-                SfActe.Statut_du_XML__c = responses[response.Key].status_xml; // <== update status for prod acte and leave it empty in acte
-                if(!responses[response.Key].isSuccessCall) SfActe.fieldsToNull = new String[] { "Date_Envoi_Prod__c" }; // purger la date pour qu'elle ne figure pas dans la Regul
-                SaveResult[] saveResults = SfService.update(new sObject[] { SfActe });
-
+                    // update data
+                    SfActe.Commentaire_XML__c = string.Join(" ", responses[response.Key].message);
+                    SfActe.Statut_du_XML__c = responses[response.Key].status_xml; // <== update status for prod acte and leave it empty in acte
+                    if (!responses[response.Key].isSuccessCall) SfActe.fieldsToNull = new String[] { "Date_Envoi_Prod__c" }; // purger la date pour qu'elle ne figure pas dans la Regul
+                    SaveResult[] saveResults = SfService.update(new sObject[] { SfActe });
+                } // must return one or zero
+                    
                 //UPDATE PROD ACTE
                 Production_Acte__c prodActe = new Production_Acte__c();
                 string soqlQueryProdActe = "SELECT Id, Statut_du_XML__c FROM Production_Acte__c WHERE Name = '" + response.Key[1] + "'";
                 result = SfService.query(soqlQueryProdActe);
-                if (result.size != 0)
+                if (result.size != 0) {
                     prodActe = (Production_Acte__c)result.records[0];
-                //prodActe.Statut_du_XML__c = responses[response.Key].status_xml;
-                saveResults = SfService.update(new sObject[] { prodActe });
+                    prodActe.Statut_du_XML__c = responses[response.Key].status_xml;
+                    SaveResult[] saveResults = SfService.update(new sObject[] { prodActe });
+                }
+
             }
         }
 
